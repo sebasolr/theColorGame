6d5ea1453c85cf2c7a5193f4c33b1d45852fd9ed
sebasolr <seba.olr@gmail.com>
2022-06-25 13:39:27 -0400

    eventListener

--- main.js
+++ main.js
@@ -1,19 +1,54 @@
 //variables globales
 let colors = ['rgb(255, 87, 51)', 'rgb(255, 177, 51)', 'rgb(122, 230, 41) ', 'rgb(30, 196, 113)', 'rgb(45, 101, 190)', 'rgb(171, 45, 190)'];
-let squares = document.querySelectorAll('.square');
+const squares = document.querySelectorAll('.squares');
 let container = document.querySelector('.container');
-
+const pickedColor = pickColor();
+let span = document.querySelector('.colorDisplay')
+span.innerHTML=pickedColor;
 //Cambio de color en etiquetas
+
+for(let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+
+
+
+
 for (let i = 0; i <= 5; i++) {
+    
     squares[i].style.backgroundColor = colors[i]
+    squares[i].addEventListener("click", function() {
+    //toma el  color de squares
+    let clickedColor = this.style.background;
+    //compara el color con pickedColor
+    if (clickedColor === pickedColor) {
+      changeColors(clickedColor);
+    }else{
+      this.style.background = "#232323";
+    }
+  })
 }
 
-function addElement(){
-    //crear un h1
-    // añadir el contenido
-    let newH1 = document.createElement("h1");
-    let newContent = document.createTextNode("The Great Guessing Game");
-    let prueba = newH1.appendChild(newContent);
-    container.appendChild(prueba)
-} 
-addElement()
+//funciones
+
+// cambiar color
+function changeColors(color){
+    //repetir en todos los squares
+  for (let i = 0; i < squares.length; i++) {
+      //cambiar el color de los squares
+      squares[i].style.background = color;
+  }
+ //color random 
+  }
+  function pickColor(){
+    var random = Math.floor(Math.random() * colors.length)
+    return colors[random];
+    }
+// funcion de crear codigo rgb random aqui
+
+
+
