fe623ac7224dc77fa2ca6ee7c816f21b219764bd
Felipe Trincado <f.trincado7@gmail.com>
2022-06-28 12:24:58 -0400

    eventos botones agregados

--- main.js
+++ main.js
@@ -1,77 +1,99 @@
 //variables globales
-let cantidadColores = 3;
-let colors =rellenarArray(cantidadColores);
+let cantidadColores = 6;
+let colors = generateRandomColors(cantidadColores);
 const squares = document.querySelectorAll('.squares');
 let container = document.querySelector('.container');
-const pickedColor = pickColor();
+let pickedColor = pickColor();
 let span = document.querySelector('.colorDisplay');
-span.innerHTML=pickedColor;
+span.innerHTML = pickedColor;
 let mensaje = document.querySelector('.mensaje');
-let boton=document.querySelector('#reset');
-let level2=document.querySelector('#level2');
+let boton = document.querySelector('#reset');
+let levelhard = document.querySelector('#hard');
+let leveleasy = document.querySelector('#easy');
 //boton reset
-
-boton.addEventListener('click',function(){
+boton.addEventListener('click', function () {
   window.location.reload()
 })
-//boton level
-level2.addEventListener('click',function(){
-  
-})
-//Cambio de color en etiquetas
-for(let i = 0; i < cantidadColores; i++) {
-  if (colors[i]) {
-    squares[i].style.background = colors[i];
-  } else {
-    squares[i].style.display = "none";
+//boton easy
+leveleasy.addEventListener('click', function () {
+  leveleasy.classList.add('selected');
+  levelhard.classList.remove('selected');  
+  cantidadColores = 3;
+  colors = generateRandomColors(cantidadColores);
+  pickedColor = pickColor();
+  span.innerHTML = pickedColor;
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
   }
-}
+})
+//btn hard
+levelhard.addEventListener('click', function () {
+  levelhard.classList.add('selected');
+  leveleasy.classList.remove('selected');
+  cantidadColores = 6;  
+  colors = generateRandomColors(cantidadColores);
+  pickedColor = pickColor();
+  span.innerHTML = pickedColor;
+  for (let i = 0; i <squares.length ; i++) {
+    
+      squares[i].style.background = colors[i];
+    
+      squares[i].style.display = "block";
+    
+  }
+})
+
 //clicker cuando el color es = al colorcliker  
 for (let i = 0; i <= 5; i++) {
   squares[i].style.backgroundColor = colors[i]
   //evento click
-  squares[i].addEventListener("click", function() {
-  //toma el  color de squares
+  squares[i].addEventListener("click", function () {
+    //toma el  color de squares
     const clickedColor = this.style.background;
-  //compara el color con pickedColor
-      if (clickedColor == pickedColor) {
-        changeColors(clickedColor);
-        mensaje.innerHTML= "Increible!!!"
-      }else{
-        this.style.background = "#232323";
-        mensaje.innerHTML= "intentalo nuevamente!"
-      } console.log(clickedColor)
+    //compara el color con pickedColor
+    if (clickedColor == pickedColor) {
+      changeColors(clickedColor);
+      mensaje.innerHTML = "¡Correcto!"
+    } else {
+      this.style.background = "#232323";
+      mensaje.innerHTML = "intentalo nuevamente!"
+      reset.innerHTML = 'Play Again?'
+    } console.log(clickedColor)
   })
 }
 //funciones
 
 // cambiar color
-function changeColors(color){
-    //repetir en todos los squares
+function changeColors(color) {
+  //repetir en todos los squares
   for (let i = 0; i < cantidadColores; i++) {
-      //cambiar el color de los squares
-      squares[i].style.background = color;
+    //cambiar el color de los squares
+    squares[i].style.background = color;
   }
- //color random 
+  //color random 
 }
-function pickColor(){
-    let random = Math.floor(Math.random() * colors.length)
-    return colors[random];
+function pickColor() {
+  let random = Math.floor(Math.random() * colors.length)
+  return colors[random];
 }
 // funcion de crear codigo rgb random aqui
 
-function randomColor (){
-  
-    let r = Math.floor(Math.random()*256)
-    let g = Math.floor(Math.random()*256)
-    let b = Math.floor(Math.random()*256)
-    return  'rgb'+'('+r+', '+g+', '+b+')'
+function randomColor() {
+
+  let r = Math.floor(Math.random() * 256)
+  let g = Math.floor(Math.random() * 256)
+  let b = Math.floor(Math.random() * 256)
+  return 'rgb' + '(' + r + ', ' + g + ', ' + b + ')'
 }
 
-function rellenarArray(){
-    let colors=[];
-    for(let i=0; i<cantidadColores;i++){
-      colors.push(randomColor())
+function generateRandomColors() {
+  let colors = [];
+  for (let i = 0; i < cantidadColores; i++) {
+    colors.push(randomColor())
   }
-return colors
+  return colors
 }
